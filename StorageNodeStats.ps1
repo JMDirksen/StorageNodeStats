96ec3bc26d886f8c1d5727e2d644292da450dbba
JMDirksen <jeftadirksen@gmail.com>
2024-07-28 11:56:25 +0200

    + TotalDisk

--- StorageNodeStats.ps1
+++ StorageNodeStats.ps1
@@ -11,6 +11,7 @@ If (-not (Test-Path 'StorageNodeStats.json')) {
 }
 $Settings = Get-Content -Path 'StorageNodeStats.json' | ConvertFrom-Json
 
+$TotalDisk = 0
 $TotalDiskUsed = 0
 $TotalBandwidthUsed = 0
 $TotalPayout = 0
@@ -19,16 +20,18 @@ $Settings.Hosts | ForEach-Object {
     $api = "http://$_/api"
     $sno = Invoke-RestMethod -Uri "$api/sno/"
     $estimatedPayout = Invoke-RestMethod -Uri "$api/sno/estimated-payout"
+    $TotalDisk += $sno.diskSpace.available
     $TotalDiskUsed += $sno.diskSpace.used + $sno.diskSpace.trash
     $TotalBandwidthUsed += $estimatedPayout.currentMonth.egressBandwidth
     $TotalPayout += $estimatedPayout.currentMonth.payout
     $TotalExpectedPayout += $estimatedPayout.currentMonthExpectations
 }
+$TotalDisk = [Math]::Round([UInt64]$TotalDisk / [Math]::Pow(10, 12), 3) # TB
 $TotalDiskUsed = [Math]::Round([UInt64]$TotalDiskUsed / [Math]::Pow(10, 12), 3) # TB
 $TotalBandwidthUsed = [Math]::Round([UInt64]$TotalBandwidthUsed / [Math]::Pow(10, 12), 3) # TB
 $TotalPayout = [Math]::Round([UInt64]$TotalPayout / 100, 2)
 $TotalExpectedPayout = [Math]::Round([UInt64]$TotalExpectedPayout / 100, 2)
-$Stats = @($TotalDiskUsed, $TotalBandwidthUsed, $TotalPayout, $TotalExpectedPayout)
+$Stats = @($TotalDisk, $TotalDiskUsed, $TotalBandwidthUsed, $TotalPayout, $TotalExpectedPayout)
 
 $Limit = $Settings.KeepDays * 24 * 60 / $Settings.IntervalMinutes
 $Stats = (@((Get-Date).ToString("yyyy-MM-dd HH:mm")) + $Stats) -join ";"
